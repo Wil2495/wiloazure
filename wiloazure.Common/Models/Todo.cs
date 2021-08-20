44a94729ccd7f226f30659d66d0c4131ddd059b5
wil2495 <alexisfranco2495@hotmail.com>
2021-08-20 11:41:08 -0500

    add entity

---
+++ wiloazure.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace wiloazure.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompletd { get; set; }
+    }
+}
---
+++ wiloazure.Common/Responses/Response.cs
@@ -0,0 +1,12 @@
+﻿namespace wiloazure.Common.Responses
+{
+    public class Response
+    {
+
+        public bool IsSuccess { get; set; }
+
+        public string Nessage { get; set; }
+
+        public object Result { get; set; }
+    }
+}
