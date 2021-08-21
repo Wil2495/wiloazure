0e4939f5b9077db714e3c1ab1c82a2505ab79582
wil2495 <alexisfranco2495@hotmail.com>
2021-08-21 14:37:47 -0500

    Unit test preparation

---
+++ wiloazure.Test/Helpers/ListLogger.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wiloazure.Test.Helpers
+{
+    public class ListLogger : ILogger
+    {
+
+        public ListLogger()
+        {
+
+            logs = new List<string>();
+        }
+
+
+        public IList<String> logs;
+
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return NullScope.Instance;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return false;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            String message = formatter(state, exception);
+            logs.Add(message);
+        }
+    }
+}
